75d66cfa7bf414815c044c9a54fb2a717f7be2a4
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-07-16 11:38:44 +0800

    feat: 修改密码 退出登录

--- src/controller/user.js
+++ src/controller/user.js
@@ -16,7 +16,8 @@ const {
 	registerFailInfo,
 	loginFailInfo,
 	deleteUserFailInfo,
-	changeInfoFailInfo
+	changeInfoFailInfo,
+	changePasswordFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -129,10 +130,43 @@ async function changeInfo(ctx, { nickName, city, picture }) {
 	return new ErrorModel(changeInfoFailInfo);
 }
 
+/**
+ * 修改密码
+ * @param  {[type]} userName    用户名
+ * @param  {[type]} password    当前密码
+ * @param  {[type]} newPassword 新密码
+ */
+async function changePassword(userName, password, newPassword) {
+	const result = await updateUser(
+		{ newPassword: doCrypto(newPassword) },
+		{
+			userName,
+			password: doCrypto(password)
+		}
+	);
+	if (result) {
+		// 返回
+		return new SuccessModel();
+	}
+	// 失败
+	return new ErrorModel(changePasswordFailInfo);
+}
+
+/**
+ * 退出登录
+ * @param  {Object} ctx ctx
+ */
+async function logout(ctx) {
+	delete ctx.session.userInfo;
+	return new SuccessModel();
+}
+
 module.exports = {
 	isExist,
 	register,
 	login,
 	deleteCurUser,
-	changeInfo
+	changeInfo,
+	changePassword,
+	logout
 }
