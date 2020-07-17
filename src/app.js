381f173adeaba994ce523a7757e2d08b939426fe
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-07-17 10:19:37 +0800

    feat: 创建微博 api

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ const blogViewRouter = require('./routes/view/blog')
 const userViewRouter = require('./routes/view/user')
 const userAPIRouter = require('./routes/api/user')
 const utilsAPIRouter = require('./routes/api/utils')
+const blogHomeAPIRouter = require('./routes/api/blog-home')
 
 // error handler
 let onerrorConf = {}
@@ -64,6 +65,7 @@ app.use(userViewRouter.routes(), userViewRouter.allowedMethods())
 app.use(blogViewRouter.routes(), blogViewRouter.allowedMethods())
 app.use(userAPIRouter.routes(), userAPIRouter.allowedMethods())
 app.use(utilsAPIRouter.routes(), utilsAPIRouter.allowedMethods())
+app.use(blogHomeAPIRouter.routes(), blogHomeAPIRouter.allowedMethods())
 app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())		// 404路由注册到最下面
 
 // error-handling
