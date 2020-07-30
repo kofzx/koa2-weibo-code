c368f4a452bb58ddc8a4d444be215989c65eebbf
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-07-30 17:20:24 +0800

    feat: 个人主页 单元测试

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -4,5 +4,6 @@
  */
 
 module.exports = {
-	COOKIE: 'weibo.sid=Gt7Q1RlhZSlmvr1PaWiwhn7tZ7TH_tKz; weibo.sid.sig=gDbwPsvuS3dgbFSGeM9d3xzdy70'
+	USER_NAME: 'lisi',
+	COOKIE: 'weibo.sid=koq4WeKCJHkn3_9S2_FECFJRu336F3kb; weibo.sid.sig=RzBJ1O0FwBNDkSzQ5rctr6RMmMI'
 }
