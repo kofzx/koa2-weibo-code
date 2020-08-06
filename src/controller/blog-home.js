a3f4a4c131a495f5d7dfa945e2759d5a0b9ac854
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-08-06 15:00:51 +0800

    feat: 首页

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -4,11 +4,10 @@
  */
 
 const xss = require('xss')
-const { createBlog } = require('../services/blog')
+const { createBlog, getFollowersBlogList } = require('../services/blog')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
-const {
-	createBlogFailInfo
-} = require('../model/ErrorInfo')
+const { createBlogFailInfo } = require('../model/ErrorInfo')
+const { PAGE_SIZE } = require('../conf/constant')
 
 /**
  * 创建微博
@@ -30,6 +29,31 @@ async function create({ userId, content, image }) {
 	}
 }
 
+/**
+ * 获取首页微博列表
+ * @param  {number} userId    userId
+ * @param  {Number} pageIndex pageIndex
+ */
+async function getHomeBlogList(userId, pageIndex = 0) {
+	const result = await getFollowersBlogList(
+		{ 
+			userId, 
+			pageIndex, 
+			pageSize: PAGE_SIZE 
+		}
+	);
+	const { count, blogList } = result;
+
+	return new SuccessModel({
+		isEmpty: blogList.length === 0,
+		blogList,
+		pageSize: PAGE_SIZE,
+		pageIndex,
+		count
+	})
+}
+
 module.exports = {
-	create
+	create,
+	getHomeBlogList
 }
